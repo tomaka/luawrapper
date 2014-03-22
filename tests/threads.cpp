482de7f8c6c5ea6b1950e8a95506eb111150f8ff
Tomaka17 <pierre.krieger1708@gmail.com>
2014-03-22 19:51:09 +0100

    Added threads tests

--- tests/threads.cpp
+++ tests/threads.cpp
@@ -6,16 +6,18 @@ TEST(Threads, Fork) {
 	context.writeVariable("a", "hello");
 
 	auto thread1 = context.createThread();
+	context.forkGlobals(thread1);
 	auto thread2 = context.createThread();
+	context.forkGlobals(thread2);
 
-	//context.executeCode(thread1, "a = 3");
-	//context.executeCode(thread2, "a = 18");
+	context.executeCode(thread1, "a = 3");
+	context.executeCode(thread2, "a = 18");
 
-	//EXPECT_EQ(3, context.readVariable<int>(thread1, "a"));
+	EXPECT_EQ(3, context.readVariable<int>(thread1, "a"));
 
-	//context.executeCode(thread1, "a = 5");
-	//context.writeVariable("a", "hello");
-	//EXPECT_EQ(18, context.readVariable<int>(thread2, "a"));
+	context.executeCode(thread1, "a = 5");
+	context.writeVariable("a", "hello");
+	EXPECT_EQ(18, context.readVariable<int>(thread2, "a"));
 
 	EXPECT_EQ("hello", context.readVariable<std::string>("a"));
 }
