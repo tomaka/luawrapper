4a7fe8a31b993f6e4ce4eba197dff545d3151bed
Tomaka17 <pierre.krieger1708@gmail.com>
2014-04-14 21:39:59 +0200

    Revert "Reader now returns values with no precision loss"
    This reverts commit 4acd4b2bffc163348f5e03caf67fd2fcdb4a533f.

--- include/LuaContext.hpp
+++ include/LuaContext.hpp
@@ -2204,7 +2204,7 @@ struct LuaContext::Reader<
 			typename std::enable_if<std::is_integral<TType>::value>::type
 		>
 {
-	using ReturnType = lua_Integer;
+	using ReturnType = TType;
 
 	static auto read(lua_State* state, int index)
 		-> boost::optional<TType>
@@ -2225,7 +2225,7 @@ struct LuaContext::Reader<
 			typename std::enable_if<std::is_floating_point<TType>::value>::type
 		>
 {
-	using ReturnType = lua_Number;
+	using ReturnType = TType;
 
 	static auto read(lua_State* state, int index)
 		-> boost::optional<TType>
